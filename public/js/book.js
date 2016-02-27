783b9f125b208f83e10ee048c6671283b1d426da
Busuttil Nicolas <nicolas.busuttil@pmsipilot.com>
2016-02-27 13:03:45 +0100

    feat(menu): Mise en place du menu, et clean public

--- public/js/book.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function($) {
-    var width = $(window).width() -40,
-        height = $(window).height() -40;
-
-    $('.container-entry').css({
-        width : width,
-        height : height
-    });
-});
